8d4f92663ff1d790d540f05d7adfd68da20500bf
Ryna Truong <rynatruong@outlook.com>
2020-05-12 21:34:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+ def badge_maker(name)
+   return "Hello, my name is #{name}."
+ end
+
+def batch_badge_creator(arr)
+  arr_of_badge_msgs = []
+  arr.each do |name|
+    arr_of_badge_msgs << badge_maker(name)
+  end
+  arr_of_badge_msgs
+end
+  
+
+def assign_rooms(speakers)
+  new_arr = []
+  speakers.each_with_index do |idx, name|
+    new_arr << speakers
+  end
+  new_arr
+end
+
+# def printer
+#   batch_badge_creator(arr)
+#   assign_rooms(speakers)
+# end
